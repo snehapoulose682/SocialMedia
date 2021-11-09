e1d4d8462c08218745cef2195a038db7dc32ed80
Sudev S <sudev5552@gmail.com>
2021-11-09 10:13:03 +0530

    - router paths setu up. -login for path / - posts page for /postspage -post details page for /postdetailspage -profile page for /profilepage

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,26 @@
-import './App.css';
+import "./index.css";
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import LoginPage from "./pages/LoginPage";
+import PostsPage from "./pages/PostsPage";
+import PostDetailsPage from "./pages/PostDetailsPage";
+import ProfilePage from "./pages/ProfilePage";
 
 function App() {
   return (
-    <div className="App">
-     {/* router goes here */}
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route exact path="/" component={LoginPage}></Route>
+          <Route exact path="/postsPage" component={PostsPage}></Route>
+          <Route
+            exact
+            path="/postDetailsPage"
+            component={PostDetailsPage}
+          ></Route>
+          <Route exact path="/profilePage" component={ProfilePage}></Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
