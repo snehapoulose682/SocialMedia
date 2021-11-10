a795545db23c1427478409af6a2bacabb3148f4a
Sudev S <sudev5552@gmail.com>
2021-11-10 13:15:31 +0530

    -setup routes to all pages -restructed the code with version 6

---
+++ src/pages/PostDetailsPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PostDetailsPage = () => {
+    return (
+        <div>
+            <h2>inside post PostDetailsPage</h2>
+        </div>
+    )
+}
+
+export default PostDetailsPage
