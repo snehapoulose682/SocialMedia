21ec2d4c543f2606acbc82a040d0e385e1ab381c
Sudev S <sudev5552@gmail.com>
2021-11-10 08:50:35 +0530

    -user <Routes> instead of <Switch>

--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,21 @@
-import "./index.css";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import LoginPage from "./pages/LoginPage";
-import PostsPage from "./pages/PostsPage";
-import PostDetailsPage from "./pages/PostDetailsPage";
-import ProfilePage from "./pages/ProfilePage";
+import '../index.css'
+import { BrowserRouter as Router,Routes } from "react-router-dom";
+import {Route} from "react-router";
+import LoginPage from "../pages/LoginPage";
+import PostsPage from "../pages/PostsPage";
+import PostDetailsPage from "../pages/PostDetailsPage";
+import ProfilePage from "../pages/ProfilePage";
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <Switch>
+        <Routes >
           <Route exact path="/" component={LoginPage} />
           <Route exact path="/postsPage" component={PostsPage} />
           <Route exact path="/postDetailsPage" component={PostDetailsPage} />
           <Route exact path="/profilePage" component={ProfilePage} />
-        </Switch>
+        </Routes >
       </div>
     </Router>
   );
